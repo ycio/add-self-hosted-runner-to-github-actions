40bed670333b38914ce2a29f6d29d6f06c57e9b6
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-12-23 21:29:34 +0800

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -euo pipefail
+
+echo "The installation script is based on the instruction from https://docs.docker.com/engine/install/ubuntu/"
+
+apt-get remove docker docker-engine docker.io containerd runc runc || true
+
+apt-get update
+
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+docker run hello-world
