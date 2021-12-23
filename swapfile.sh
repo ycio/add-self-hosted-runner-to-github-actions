e5f7003ae3e2b3844782056fa4e9d827aa7bf395
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-12-23 23:10:39 +0800

    Create swapfile.sh

---
+++ swapfile.sh
@@ -0,0 +1,10 @@
+
+#! /bin/bash
+
+set -euo pipefail
+
+dd if=/dev/zero of=/swapfile count=4096 bs=1M
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
