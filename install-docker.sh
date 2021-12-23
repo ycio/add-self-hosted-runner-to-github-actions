12c9b7d970e1aba681a36b63c9116cf28d16a17c
Yuan Zhang <zhangyuan@users.noreply.github.com>
2021-12-23 22:31:47 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -14,7 +14,7 @@ apt-get install -y \
     gnupg \
     lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
